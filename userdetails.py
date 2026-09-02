f5d456ab69d2300308c6b858c1940af9075af0a9
Archana-24cell <archana133566@gmail.com>
2026-09-02 14:23:53 +0530

    save the file

--- userdetails.py
+++ userdetails.py
@@ -3,3 +3,7 @@ lastname = input("Enter your last name")
 city = input("Enter your city")
 contact = input("Enter user contact")
 
+print("user first name and last name",firstname +""+lastname)
+print("user city",city)
+print("user contact",contact)
+
